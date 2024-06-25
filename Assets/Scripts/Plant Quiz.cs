f867c73a023ab819945144a3ef43d0ff1471ce0c
Fayhee <96606732+PatMekury@users.noreply.github.com>
2024-06-25 14:49:24 -0500

    Update of the bird Animation

--- Assets/Scripts/Plant Quiz.cs
+++ Assets/Scripts/Plant Quiz.cs
@@ -1,18 +1,77 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantQuiz : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject quizCanvas;
+    public GameObject quizGame;
+    public GameObject correctCanvas;
+
+    public Button submitButton;
+    public Button[] imageButtons;
+
+    private int correctAnswerIndex = 1; // Index of the correct image button
+    private int selectedAnswerIndex = -1; // Index of the selected image button
+
     void Start()
     {
-        
+        foreach (Button button in imageButtons)
+        {
+            button.onClick.AddListener(() => OnImageButtonClick(button));
+        }
+
+        submitButton.onClick.AddListener(OnSubmitButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnImageButtonClick(Button clickedButton)
     {
-        
+        // Deselect all buttons
+        foreach (Button button in imageButtons)
+        {
+            button.GetComponent<Image>().color = Color.white;
+        }
+
+        // Select the clicked button
+        clickedButton.GetComponent<Image>().color = Color.green;
+        selectedAnswerIndex = System.Array.IndexOf(imageButtons, clickedButton);
+    }
+
+    void OnSubmitButtonClick()
+    {
+        if (selectedAnswerIndex == correctAnswerIndex)
+        {
+            StartCoroutine(ShowCorrectCanvas());
+        }
+        else
+        {
+            StartCoroutine(ShowIncorrectSelection());
+        }
+    }
+
+    IEnumerator ShowCorrectCanvas()
+    {
+        quizGame.SetActive(false);
+        correctCanvas.SetActive(true);
+        yield return new WaitForSeconds(5);
+        quizCanvas.SetActive(false);
+        // Proceed to the next question or end the game
+    }
+
+    IEnumerator ShowIncorrectSelection()
+    {
+        imageButtons[selectedAnswerIndex].GetComponent<Image>().color = Color.red;
+        yield return new WaitForSeconds(2);
+        ResetButtons();
+        quizCanvas.SetActive(true);
+    }
+
+    void ResetButtons()
+    {
+        foreach (Button button in imageButtons)
+        {
+            button.GetComponent<Image>().color = Color.white;
+        }
+        selectedAnswerIndex = -1;
     }
 }
